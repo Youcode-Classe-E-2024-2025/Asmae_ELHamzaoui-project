31e42b955a4317b457388b85e63a776a9c7a09a9
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-01-06 10:05:03 +0100

    update pour le controleur de projet

--- controllers/projetController.php
+++ controllers/projetController.php
@@ -8,16 +8,16 @@ class ProjetController {
         $this->projetModel = new Projet($db);
     }
     // Ajouter un projet
-    public function ajouterProjet($nom, $desc, $date_debut, $date_fin, $visibilite) {
-        if ($this->projetModel->ajouterProjet($nom, $desc, $date_debut, $date_fin, $visibilite)) {
+    public function ajouterProjet($nom, $desc, $date_debut, $date_fin, $visibilite, $photo_Projet) {
+        if ($this->projetModel->ajouterProjet($nom, $desc, $date_debut, $date_fin, $visibilite, $photo_Projet)) {
             echo "Projet ajouté avec succès.";
         } else {
             echo "Erreur lors de l'ajout du projet.";
         }
     }
     // Modifier un projet
-    public function modifierProjet($id, $nom, $desc, $date_debut, $date_fin, $visibilite) {
-        if ($this->projetModel->modifierProjet($id, $nom, $desc, $date_debut, $date_fin, $visibilite)) {
+    public function modifierProjet($id, $nom, $desc, $date_debut, $date_fin, $visibilite, $photo_Projet) {
+        if ($this->projetModel->modifierProjet($id, $nom, $desc, $date_debut, $date_fin, $visibilite, $photo_Projet)) {
             echo "Projet modifié avec succès.";
         } else {
             echo "Erreur lors de la modification du projet.";
