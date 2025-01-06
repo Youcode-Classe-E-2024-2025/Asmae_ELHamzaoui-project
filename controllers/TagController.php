b4cb11d48cc57122abd15ece5b16c3f40281d726
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-01-06 15:35:46 +0100

    création de fichier TagController.php et l'implémentation du code pour l'insertion d'une nouvelle tag

---
+++ controllers/TagController.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'Model/Tag.php';
+
+class TagController
+{
+    private $tagModel;
+
+    public function __construct($db)
+    {
+        $this->tagModel = new Tag($db);
+    }
+
+    public function createTag()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $nomTag = $_POST['nom_tag'];
+            $descTag = $_POST['desc_tag'];
+
+            if ($this->tagModel->createTag($nomTag, $descTag)) {
+                echo json_encode(['success' => true]);
+            } else {
+                echo json_encode(['success' => false]);
+            }
+        }
+    }
+}
