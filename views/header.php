9f217b40aac175969bd86e20ad4ae00ea3f3f7ee
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-01-07 16:39:14 +0100

    mettre le header dans un seul fichier

---
+++ views/header.php
@@ -0,0 +1,23 @@
+<header class="mx-4">
+        <div class="container flex justify-between items-center">
+            <!-- Logo avec taille augmentée -->
+            <img src="images/logo.png" alt="Logo" class="h-24 w-32"> <!-- Ajout de la classe "logo" pour appliquer la transformation -->
+            <!-- Menu & Icons avec un espacement égal entre les éléments -->
+            <div  class="flex space-x-8 items-center">
+                <a href="index.php" class="text-2xl font-bold hover:text-gray-400" style="color:#24508c">Home</a>
+                <a href="views/publicProjects.php" class="text-2xl font-bold hover:text-gray-400" style="color:#24508c">Project</a>
+                <a href="#" class="text-2xl font-bold hover:text-gray-400" style="color:#24508c">About</a>
+            </div>
+            <div class="flex space-x-6 items-center mr-8"> <!-- Espacement égal entre les éléments -->
+                <!-- Icône pour l'inscription -->
+                <a href="views/direction.php" class="text-2xl  hover:text-gray-400" style="color:#24508c;font-weight:600;">
+                   Sign up
+                </a>
+                
+                <!-- Icône pour la connexion -->
+                <a href="views/login.php" class="text-2xl  hover:text-gray-400" style="color:#24508c ;font-weight:600;">
+                    log in
+                </a>
+            </div>
+        </div>
+    </header>
